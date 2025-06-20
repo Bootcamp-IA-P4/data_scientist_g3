7d2a467e5691a5f4a7cbcf69bb70c148b38f68b7
Maximiliano Scarlato <maximilianocsc2@gmail.com>
2025-06-20 16:58:24 +0200

    feat(frontend): update app.py and env example - closes #27

--- frontend/app.py
+++ frontend/app.py
@@ -696,8 +696,10 @@ def handle_page_errors(pathname):
             ], className="error-page-content")
         ])
 
-#if __name__ == '__main__':
-#    app.run(debug=True, host='127.0.0.1', port=FRONTEND_PORT)
-
+# SOLO DESCOMENTA PARA EJECUTAR LOCALMENTE
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=FRONTEND_PORT)# para docker
+    app.run(debug=True, host='127.0.0.1', port=FRONTEND_PORT)
+
+# SOLO DESCOMENTA PARA DOCKERIZAR
+# if __name__ == '__main__':
+#     app.run(debug=True, host='0.0.0.0', port=FRONTEND_PORT)
